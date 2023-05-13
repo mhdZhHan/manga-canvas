a262c3dd86efde31b7144ebea92f195ec6c840e7
Mohammed Shajahan <mohammedsh6236@gmail.com>
2023-05-13 23:50:06 +0530

    Something new

--- src/components/common/cards/ComicCard.js
+++ src/components/common/cards/ComicCard.js
@@ -1,14 +1,17 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import { styles } from './comiccard.style'
 
 const ComicCard = ({ data, index, arrLength }) => {
     return (
         <View style={styles.cardContainer(index, arrLength)}>
-            <Image 
-                source={data.image}
-                resizeMode='cover'
-                style={styles.cardImage}
-            />
+            <TouchableOpacity activeOpacity={.8}>
+                <Image 
+                    source={data.image}
+                    resizeMode='cover'
+                    style={styles.cardImage}
+                />
+            </TouchableOpacity>
+            
             <Text style={styles.cardTitle}>Trending Comix</Text>
             <Text style={styles.cardAuthor}>Good morning</Text>
         </View>
